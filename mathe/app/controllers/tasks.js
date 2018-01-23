450fd3116bf954671bbc6c684f7020a062a9d630
Frank Dressel <fdressel@users.noreply.github.com>
2018-01-23 22:11:41 +0100

    Fixed bug.

--- mathe/app/controllers/tasks.js
+++ mathe/app/controllers/tasks.js
@@ -11,13 +11,15 @@ export default Ember.Controller.extend({
             let results=this.get('model').get('results').map(function(e){return e;});
             let counter=results.reduce(function(p, c){return p+(c.result?1:0)}, 0);
             if(results[counter-1].result==results[counter-1].third){
-                results[counter-1].set('correct', true);
+                results[counter-1].correct=true;
             }
             else{
-                results[counter-1].set('correct', false);
+                results[counter-1].correct=false;
             }
 
-            this.get('model').set('results', results);
+            let modelresults=this.get('model').get('results');
+            modelresults.clear();
+            results.forEach(function(e){modelresults.pushObject(Ember.copy(e));});
             this.get('model').set('counter', counter);
             if(counter==this.get('model').get('results').length){
                 this.get('model').set('done', true);
@@ -38,7 +40,7 @@ export default Ember.Controller.extend({
         }
 
         model.set('counter', 0);
-        for(let i=0;i<model.results.length;i++){
+        for(let i=0;i<10;i++){
             let first=0;
             let second=0;
             let third=0;
@@ -56,14 +58,13 @@ export default Ember.Controller.extend({
                     break;
                 }
             }
-            model.get('results')[i]={
+            model.get('results').pushObject({
                 first: first,
                 second: second,
                 third: third,
                 result: 0,
                 plusminus: plusminus,
-                correct: null
-            };
+            });
         }
     },
 });
--- mathe/app/routes/tasks.js
+++ mathe/app/routes/tasks.js
@@ -4,7 +4,7 @@ const Task = Ember.Object.extend({})
 export default Ember.Route.extend({
     model() {
         return Task.create({
-            results: Array(10).fill(null),
+            results: [],
             startState: true,
             berechnetState: false
             });
