043c2a0bb55835590658624561dddfa42a5776ed
Frank Dressel <frankdressel@users.noreply.github.com>
2018-04-17 22:23:35 +0200

    Added root url.

--- sudoku/config/environment.js
+++ sudoku/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'sudoku',
     environment,
-    rootURL: '/',
+    rootURL: '/games/sudoku',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
