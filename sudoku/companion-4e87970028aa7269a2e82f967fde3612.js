ed85a73a8ffc87518fa82b502280af36fec2f6a7
Frank Dressel <frankdressel@users.noreply.github.com>
2018-04-22 21:41:14 +0200

    Offline

---
+++ sudoku/companion-4e87970028aa7269a2e82f967fde3612.js
@@ -0,0 +1,3 @@
+(function(){"use strict"
+var r=document.currentScript&&document.currentScript.dataset.serviceWorker
+r&&"serviceWorker"in navigator&&navigator.serviceWorker.register(r)})()
---
+++ sudoku/service-worker-ff7d8b1688c29f14765f14680601258e.js
@@ -0,0 +1,1 @@
+importScripts("sw-toolbox-354b6d888daa7218a479d8e1a3271a73.js"),toolbox.router.get("*",toolbox.networkFirst)
