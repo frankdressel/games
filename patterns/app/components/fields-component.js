4d0c0e1db771aef5d074ac7da6f9107ccbb1cc02
Frank Dressel <frankdressel@users.noreply.github.com>
2018-10-29 23:18:14 +0000

    First model.

--- patterns/app/components/fields-component.js
+++ patterns/app/components/fields-component.js
@@ -7,35 +7,17 @@ export default Ember.Component.extend({
         this.set('width', 960);
         this.set('height', 500);
     },
-    didRender(){
+    fillFunction(d){
+        return d3.interpolateOranges(d['noise']['value']);
+    },
+    didInsertElement(){
         let that = this;
-        function convert(cube){
-            let result = [];
-            let w = that.get('size') * Math.sqrt(3);
-            for(let i = 0; i < 7; i++){
-                var angle_deg = 60 * i - 30;
-                var angle_rad = Math.PI / 180 * angle_deg;
-                result.push([
-                    w * cube[0] + 1/2 * w * cube[2] + that.get('size') * Math.cos(angle_rad),
-                    (2 - Math.cos(Math.PI / 180 * 60))* that.get('size') * cube[2]  + that.get('size') * Math.sin(angle_rad)
-                ]);
-            }
 
-            return result;
-        }
         let svg = d3.select('#fields').append('svg').attr('width', this.get('width')).attr('height', this.get('height'));
         let values = Array.from(that.get('data').values());
 
-        function fillFunction(d){
-            if(d.value == 1){
-                return 'orange';
-            }
-            return 'white';
-        }
-
         svg.
             selectAll('path').
-            //data(Array.from(data).map(([key,value]) => convert(key))).
             data(values).
             enter().
             append('path').
@@ -55,13 +37,38 @@ export default Ember.Component.extend({
             attr('transform', function(d) {
                 return 'translate(' + that.get('width') / 2 + ',' + that.get('height') / 2 + ')'
             }).
+            on('click', function(d, i){
+                that.get('clicked')(d, that.get('data'));
+                //d['value'] = (d['value'] + 1) % 2;
+                d3.select(this).attr('fill', that.get('fillFunction')(d));
+            });
+    },
+    didRender(){
+        let that = this;
+
+        function convert(cube){
+            let result = [];
+            let w = that.get('size') * Math.sqrt(3);
+            for(let i = 0; i < 7; i++){
+                var angle_deg = 60 * i - 30;
+                var angle_rad = Math.PI / 180 * angle_deg;
+                result.push([
+                    w * cube[0] + 1/2 * w * cube[2] + that.get('size') * Math.cos(angle_rad),
+                    (2 - Math.cos(Math.PI / 180 * 60))* that.get('size') * cube[2]  + that.get('size') * Math.sin(angle_rad)
+                ]);
+            }
+
+            return result;
+        }
+        let svg = d3.select('svg');
+        let values = Array.from(that.get('data').values());
+
+        svg.
+            selectAll('path').
+            data(values).
             attr('stroke', function(d) {
                 return 'red';
             }).
-            attr('fill', d => fillFunction(d)).
-            on('click', function(d, i){
-                d['value'] = (d['value'] + 1) % 2;
-                d3.select(this).attr('fill', fillFunction(d));
-            });
+            attr('fill', d => that.get('fillFunction')(d));
     }
 });
--- patterns/app/routes/application.js
+++ patterns/app/routes/application.js
@@ -1,23 +1,72 @@
 import Ember from 'ember';
+import EmberObject from '@ember/object';
 
 export default Ember.Route.extend({
+    modelObj: EmberObject.extend({'data': new Map(), 'time': 0}).create(),
     actions:{
         next(data){
+            let that = this;
+            let neighbors = [[1, 0, -1], [1, -1, 0], [0, -1, 1], [-1, 0, 1], [-1, 1, 0], [0, 1, -1]];
+            this.get('modelObj').set('time', this.get('modelObj').get('time') + 1);
+            let time = this.get('modelObj').get('time');
+            for (let key of data.keys()) {
+                let cell = data.get(key);
+
+                if(cell['noise']['time'][time]){
+                    cell['noise']['value'] = cell['noise']['value'] + cell['noise']['time'][time];
+                }
+                delete cell['noise']['time'][time];
+            }
             console.log(Array.from(data).filter(([key, value])=>value['value']==1));
+        },
+        noise(cell, data){
+            function dist(cell1, cell2){
+                return Math.max(
+                    Math.abs(cell1['x']-cell2['x']),
+                    Math.abs(cell1['y']-cell2['y']),
+                    Math.abs(cell1['z']-cell2['z'])
+                );
+            }
+            let size = 8;
+            let time = this.get('modelObj').get('time');
+            for(let z = -size; z <= size; z = z + 1){
+                for(let x = -size; x <= size; x = x + 1){
+                    let y = -(x+z);
+                    if(Math.abs(y)>size){
+                        continue;
+                    }
+
+                    let key = JSON.stringify([x, -(x + z), z]);
+                    let cell2 = data.get(key);
+                    if(cell2){
+                        let d = dist(cell, cell2);
+                        cell2['noise']['time'][time + d] = (cell2['noise']['time'][time + d] | 0) + 1 / d;
+                        cell2['noise']['time'][time + d + 1] = (cell2['noise']['time'][time + d + 1] | 0) - 1 / d;
+                    }
+                }
+            }
         }
     },
     model(){
-        let data = new Map();
-        let size = 8
+        let data = this.get('modelObj').get('data');
+        let size = 8;
         for(let z = -size; z <= size; z = z + 1){
             for(let x = -size; x <= size; x = x + 1){
                 let y = -(x+z);
                 if(Math.abs(y)>size){
                     continue;
                 }
-                data.set([x, -(x + z), z], {'x': x, 'y': y, 'z': z, 'value': 0});
+                let key = JSON.stringify([x, -(x + z), z]);
+                data.set(key, {
+                    'x': x,
+                    'y': y,
+                    'z': z,
+                    'value': 0,
+                    'noise':{'value': 0, 'time': {}},
+                    'cars': {},
+                });
             }
         }
-        return {'data': data};
+        return this.get('modelObj');
     }
 });
