39a72db31a6c36b8cd8630986968e0219f89cce6
Frank Dressel <frankdressel@users.noreply.github.com>
2020-02-11 00:30:17 +0000

    Updated tests

--- mathe/tests/integration/components/plusminus-component-test.js
+++ mathe/tests/integration/components/plusminus-component-test.js
@@ -1,15 +1,12 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { findAll, render } from '@ember/test-helpers';
+import { click, find, findAll, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | plusminus-component', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     let route = this.owner.lookup('route:tasks');
     let model = route.model();
     this.set('model', model);
@@ -22,6 +19,101 @@ module('Integration | Component | plusminus-component', function(hooks) {
       </PlusminusComponent>
     `);
 
+    assert.ok(true);
+  });
+
+  test('it show success markers', async function(assert) {
+    let route = this.owner.lookup('route:tasks');
+    let model = route.model();
+    this.set('model', model);
+
+    await render(hbs`<PlusminusComponent @model={{model}}/>`);
+
     assert.equal(findAll('.success-marker').length, model.results.length);
+  });
+
+  test('it shows 6 digit number fields with input', async function(assert) {
+    let route = this.owner.lookup('route:tasks');
+    let model = route.model();
+    this.set('model', model);
+
+    await render(hbs`<PlusminusComponent @model={{model}}/>`);
+
+    assert.equal(findAll('.first').length, 6);
+    assert.equal(findAll('.second').length, 6);
+    assert.equal(findAll('.third').length, 6);
+
+    for(let e of findAll('.third')) {
+      assert.equal(e.nodeName, 'INPUT');
+      assert.ok(!e.disabled);
+    }
+  });
+
+  test('it shows an button to submit the result', async function(assert) {
+    assert.expect(3);
+
+    let route = this.owner.lookup('route:tasks');
+    let model = route.model();
+    this.set('model', model);
+
+    this.set('korrigiert', function() {
+      assert.ok(true);
+    });
+    this.set('berechnet', function() {
+      assert.ok(true);
+    });
+
+    await render(hbs`<PlusminusComponent @model={{model}} @berechnet={{action berechnet}} @korrigiert={{action korrigiert}}/>`);
+
+    let button = find('.ready');
+    assert.ok(button);
+
+    click(button);
+
+    click(button);
+  });
+
+  test('input is set when calling korrigiert function', async function(assert) {
+    assert.expect(3);
+
+    let route = this.owner.lookup('route:tasks');
+    let model = route.model();
+    this.set('model', model);
+
+    this.set('korrigiert', function() {
+      assert.equal(model.results[model.counter].third, 123456);
+    });
+    this.set('berechnet', function() {
+      assert.ok(true);
+    });
+
+    await render(hbs`<PlusminusComponent @model={{model}} @berechnet={{action berechnet}} @korrigiert={{action korrigiert}}/>`);
+
+    let button = find('.ready');
+    assert.ok(button);
+    this.set('thirdnumbers', '123456');
+
+    click(button);
+
+    click(button);
+  });
+
+  test('success-marker shows task after submit', async function(assert) {
+    let route = this.owner.lookup('route:tasks');
+    let model = route.model();
+    this.set('model', model);
+    model.results[0].correct = true;
+
+    await render(hbs`<PlusminusComponent @model={{model}}/>`);
+
+    let exp = model.results[0].first + model.results[0].plusminus + model.results[0].second;
+    assert.equal(findAll('.success-marker')[0].textContent, exp);
+
+    model.results[0].correct = false;
+
+    await render(hbs`<PlusminusComponent @model={{model}}/>`);
+
+    let exp = model.results[0].first + model.results[0].plusminus + model.results[0].second;
+    assert.equal(findAll('.success-marker')[0].textContent, exp);
   });
 });
